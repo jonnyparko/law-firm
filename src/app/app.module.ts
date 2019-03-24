609720bd8095a99587444bcfbabddcc389073f5e
Jonathan Parker <parker.m.jon@gmail.com>
2019-03-24 16:47:15 -0500

    setup commplete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ContactComponent,
-    AboutComponent
+    AboutComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
